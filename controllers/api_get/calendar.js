4d08cd18d048c2967c9603ed69cc99c1150d2fb5
Mazen ALkhadra <moim93@live.com>
2020-05-06 14:43:03 +0300

    Fix Bugs

--- controllers/api_get/calendar.js
+++ controllers/api_get/calendar.js
@@ -7,14 +7,14 @@ module.exports = (app, passport) => {
   app.get('/calendar/gregTohijri/:gregYear/:gregMonth/:gregDay', (req, res) => {
       const {gregYear, gregMonth, gregDay} = req.params;
       const aladanApiUrl = 
-        `http://api.aladhan.com/v1/gToH?date=${gregDay}-${gregMonth}-${gregYear}`;
+        `http://api.aladhan.com/v1/gToH?date=${gregDay}-${gregMonth + 1}-${gregYear}`;
       
       fetch(aladanApiUrl)
       .then(response => {
         res.status(response.status);
         response.json().then(({data}) => res.json({
           year: data.hijri.year,
-          month: data.hijri.month.number,
+          month: data.hijri.month.number - 1,
           day: data.hijri.day
         }))
       });
--- controllers/api_get/prayers.js
+++ controllers/api_get/prayers.js
@@ -6,8 +6,6 @@ module.exports = (app, passport) => {
 
   app.get('/prayersTimes/all', (req, res) => {
     let prayersTimesApiRes = null;
-    let prayerTimesApiUrl = 
-      `https://api.pray.zone/v2/times/today.json?ip=${req.ipAddr}&school=4`;
        
     res.status(500);
   
@@ -26,43 +24,80 @@ module.exports = (app, passport) => {
           );
           return;
         }
+       
+        fetchPrayerTimesFromPrayZone([...prayers[0]], req)
+        .then(prays => res.status(200).json(prays))
+        .catch(status => {});
 
-      fetch(prayerTimesApiUrl)
-      .then(response => {
-        res.status(response.status);
-        response.json()
+        fetchPrayerTimesFromIslamicFinder([...prayers[0]], req)
+        .then(prays => res.status(200).json(prays))
+        .catch(status => {});
+
+      
+      });
+
+  });
+
+};
+
+const fetchPrayerTimesFromPrayZone = (dbPrays, req) => {
+  let prayerTimesApiUrl = 
+      `https://api.pray.zone/v2/times/today.json?ip=${req.ipAddr}&school=4`;
+
+  return fetch(prayerTimesApiUrl)
+    .then(response => {
+      if(response.status < 200 || response.status >= 400)
+        throw response.status;
+      return response.json()
+      .then(({results}) => {
+        prayersTimesApiRes = results.datetime[0].times;
+        for (prayer of dbPrays) {
+          let prayerApiTime = prayersTimesApiRes[prayer.apiKeyName] || '';
+          prayer.athanHour = prayerApiTime.substr(0, 2);
+          prayer.athanMinutes = prayerApiTime.substr(3, 2);
+        }
+        return dbPrays;
+      })
+      .catch((err) => {
+        mojamma.log (
+          `Error in fetch from pray zone`,
+          mojamma.logLevels.SERVER_API_ERR,
+          __filename,
+          "app.get(/prayersTimes/all)",
+          null, err
+        );
+        throw 500;
+      });
+    });
+}
+
+const fetchPrayerTimesFromIslamicFinder = (dbPrays, req) => {
+  let prayerTimesApiUrl = 
+      `http://www.islamicfinder.us/index.php/api/prayer_times?user_ip=${req.ipAddr}&time_format=0&method=4`;
+
+  return fetch(prayerTimesApiUrl)
+    .then(response => {
+      if(response.status < 200 || response.status >= 400)
+        throw response.status;
+      return response.json()
         .then(({results}) => {
-          prayersTimesApiRes = results.datetime[0].times;
-          for (prayer of prayers[0]) {
+          prayersTimesApiRes = results;
+          for (prayer of dbPrays) {
             let prayerApiTime = prayersTimesApiRes[prayer.apiKeyName] || '';
             prayer.athanHour = prayerApiTime.substr(0, 2);
             prayer.athanMinutes = prayerApiTime.substr(3, 2);
           }
-          res.status(200).json(prayers[0]);
+          return dbPrays;
         })
         .catch((err) => {
-          res.status(500).end();
           mojamma.log (
-            `Error in fetch prayerTimes`,
+            `Error in fetch from islamic finder`,
             mojamma.logLevels.SERVER_API_ERR,
             __filename,
             "app.get(/prayersTimes/all)",
             null, err
           );
+          throw err;
         });
-        
-        mojamma.log (
-          `Response from prayers api: ${prayerTimesApiUrl}`,
-          mojamma.logLevels.SERVER_API_INFO,
-          __filename,
-          "app.get(/prayersTimes/all)",
-          null, response
-        ); 
-
-      });
-
     });
-
-  });
-
-};
+}
