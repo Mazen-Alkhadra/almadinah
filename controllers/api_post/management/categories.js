1ca63f94e368d36e21623dda803ff3698efca238
Mazen ALkhadra <moim93@live.com>
2020-05-18 02:11:58 +0300

    Add category manag api

--- controllers/api_post/management/categories.js
+++ controllers/api_post/management/categories.js
@@ -29,4 +29,35 @@ module.exports = (app) => {
         res.status(200).json({});
     });
   });
+
+  app.post('/management/update/category', (req, res) => {
+    let {
+      displayName,
+      type,
+      imgURL,
+      categoryId
+    } = req.body;
+    
+    imgURL = imgURL || null;
+    type = type || null;
+    displayName = displayName || null;
+
+    dbConnect.query (
+      'CALL prcUpdateCategory(?, @out_category_id);',
+      [[req.userLangPref, displayName, type, imgURL, categoryId]],
+      function (err) {
+        if(err) {
+          res.status(500).end();
+          mojamma.log (
+            `Error in Execution SQL Query: ${this.sql}\n` + err.message,
+            mojamma.logLevels.DB_ERR,
+            __filename,
+            "app.post(/management/update/category/)",
+            null, err
+          );
+          return;
+        }
+        res.status(200).json({});
+    });
+  });
 };
