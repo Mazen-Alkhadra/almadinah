a7ebeeecdcd7057ff20df3f2164beab35d7ff693
unknown <mazen.alkhadra@outlook.com>
2020-06-06 21:41:29 +0300

    add privacy policy page

--- controllers/api_get/views/index.js
+++ controllers/api_get/views/index.js
@@ -3,8 +3,12 @@ var fs = require ('fs');
 module.exports = (app, passport) => {
   
   app.get('/html/privacy-policy', (req, res) => {
-    fs.readFile('../../../views/privacy-policy/index.html', 
+    fs.readFile('../../../views/privacy-policy/index.html',
     function (err, data) {
+      if(err) {
+        res.status(500).end(err);
+        return;
+      }
       res.end(data);
     });
   });
