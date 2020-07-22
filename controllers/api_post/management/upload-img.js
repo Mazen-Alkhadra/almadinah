3df31f1c7dcd95114ab945a5b50934e5af7f9d99
Mazen Alkhadra <moim93@live.com>
2020-07-22 14:06:03 +0300

    Add upload img api

--- controllers/api_post/management/upload-img.js
+++ controllers/api_post/management/upload-img.js
@@ -4,11 +4,14 @@ const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' });
 
 module.exports = (app) => {
-  app.post('/upload/img', function(req, res) {
+  app.post('/upload/img', upload.single('avatar'), function(req, res) {
     //console.log(req);
-    upload.single('avatar')(req, res, function(err){
-      console.log(JSON.stringify(err));
-      console.log(req.file);
-    })
+    // upload.single('avatar')(req, res, function(err){
+    //   console.log(JSON.stringify(err));
+    //   console.log(req.file);
+    // })
+
+    console.log(req.file);
+    res.status(200).end();
   });
 };
