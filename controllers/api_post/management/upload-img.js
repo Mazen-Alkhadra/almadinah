1a972e39aea7d4be22b3e34602297548fe738701
Mazen Alkhadra <moim93@live.com>
2020-07-23 11:26:27 +0300

    Add ftp client

--- controllers/api_post/management/upload-img.js
+++ controllers/api_post/management/upload-img.js
@@ -2,12 +2,25 @@ const mojamma = require('../../../bin/mojammaa');
 const dbConnect = require('../../../database/connect');
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' });
+const ftpClient = new require('ftp')();
 
 module.exports = (app) => {
   app.post('/management/upload/img', upload.single('picture'), function(req, res) {
+  
+  ftpClient.on('ready', function() {
+    console.log('FTP Ready');
+  ftpClient.put(req.file.path, req.file.originalname, function(err) {
+    if (err) 
+      console.log(err);
 
-    
-
+      ftpClient.end();
+    });
+  });
+  ftpClient.connect({
+    host: 'ftp.scacademy.no',
+    user: 'ss-6d5c52db07257210@scacademy.no',
+    password: 'mM@12345'
+  });
 
     console.log(req.file);
     res.status(200).json({});
