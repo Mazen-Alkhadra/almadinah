f6a7a94e4d6cd30ef6f0b9625a376e9e69e614d4
Mazen Alkhadra <moim93@live.com>
2021-04-28 11:44:49 +0300

    Fix

--- firebase/fcm_msg.js
+++ firebase/fcm_msg.js
@@ -70,7 +70,7 @@ function FcmMsg ( regToken, data, topic, condition,
     this.notification = {};
     this.notification.title = notificationTitle;
     this.notification.body = notificationBody;
-    this.notification.imgURL = notificationImgUrl;
+    this.notification.data.imgURL = notificationImgUrl;
 
     if(topic)
         this.topic = topic;
